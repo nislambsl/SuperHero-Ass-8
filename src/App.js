e09a67a2651b7856c0252ead70d1a3b03b412c98
nislambsl <nazrulbsl7@gmail.com>
2021-10-17 13:32:34 +0600

    JSON file created

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Contents from './components/Contents/Contents';
-import Persons from './components/Persons/Persons';
-import Card from './components/Card/Card';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Contents></Contents>
-      <Persons></Persons>
-      <Card></Card>
-    </div>
+     </div>
   );
 }
 
--- src/components/Contents/Contents.js
+++ src/components/Contents/Contents.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Contents.css';
 
+
+
+
 const Contents = () => {
+    const [persons, setPersons] = useState([]);
+    
+    useEffect(()=>{
+        fetch('./fakeData.json')
+        .then(res => res.json())
+        .then(data=> console.log(data));
+    } , [])
+
     return (
-        <div className="content-div">
-            <h2>This is contents</h2>
+        <div className="content-container">
+             <div className="person-container">
+                 <h3>Persons </h3>
+                
+             </div>
+             <div className="card-container">
+                 <h3>Card</h3>
+             </div>
         </div>
     );
 };
